862212eb675b09cdc0acf2fe2a1c46b6f0730c86
Arcadius Kazimierski Javascript Node.js Angular.js MongoDB Express.js C# .Net SQL Server Java Android Python <arcadiusk@gmail.com>
2015-05-30 22:35:28 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/a/catinthehat/MainActivity.java
+++ app/src/main/java/com/example/a/catinthehat/MainActivity.java
@@ -104,6 +104,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
+        //no
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
