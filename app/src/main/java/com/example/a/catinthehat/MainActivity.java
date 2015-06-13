a089d95e48407ca819c0ede4dfd7f56968fe98bf
Arcadius Kazimierski Javascript, Node.js, MongoDB, Angular.js, Express.js, C# .Net, SQL Server, Java, Android, Python, Microsoft Azure, Amazon Web Services, Google Cloud Platform, Heroku, Firebase <arcadiusk@gmail.com>
2015-06-13 15:29:06 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/a/catinthehat/MainActivity.java
+++ app/src/main/java/com/example/a/catinthehat/MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends ActionBarActivity {
             public void onClick(View v) {
                 // Do something in response to button click
                 Context context = getApplicationContext();
-                Toast toast = Toast.makeText(context, getString(R.string.buttonSallyText), Toast.LENGTH_SHORT);
+                Toast toast = Toast.makeText(context, getString(R.string.buttonSallyText), Toast.LENGTH_LONG);
                 toast.show();
             }
         });
